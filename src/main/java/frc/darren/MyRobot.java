8f0260a7f6674d949faefbad69ee323ce37c870e
Jon Woodard <jwoodard@lakeshoreps.org>
2020-01-15 17:46:57 -0500

    Initial Commit to set up files - jwood

---
+++ src/main/java/frc/darren/MyRobot.java
@@ -0,0 +1,60 @@
+package frc.darren;
+
+public class MyRobot
+{
+    public void robot()
+    {
+
+    }
+
+    public void robotInit()
+    {
+
+    }
+
+    public void robotPeriodic()
+    {
+
+    }
+
+    public void autonomousInit()
+    {
+
+    }
+
+    public void autonomousPeriodic()
+    {
+
+    }
+
+    public void teleopInit()
+    {
+
+    }
+
+    public void teleopPeriodic()
+    {
+
+    }
+
+    public void testInit()
+    {
+
+    }
+
+    public void testPeriodic()
+    {
+
+    }
+
+    public void disabledInit()
+    {
+
+    }
+
+    public void disabledPeriodic()
+    {
+
+    }
+
+}
