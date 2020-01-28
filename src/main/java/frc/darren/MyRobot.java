50a658a5b2eb542695c71c228c7464eda75c9b7c
Team 4237 Laptop 4 <lakeshoreroboticsteam@gmail.com>
2020-01-27 20:25:26 -0500

    Darren: Tested Wrist methods and modified methods

--- src/main/java/frc/darren/MyRobot.java
+++ src/main/java/frc/darren/MyRobot.java
@@ -6,12 +6,13 @@ public class MyRobot
 {
     private static Wrist wrist = Wrist.getInstance();
     private static Xbox xbox = new Xbox(0);
+    
+    double newTimeOut = 1.0;
 
     public void robot()
     {
         System.out.println(this.getClass().getName() + " : Started Constructor");
         System.out.println("*** DARREN's Test Code ***");
-
         System.out.println(this.getClass().getName() + " : Finished Constructor");
     }
 
@@ -37,23 +38,83 @@ public class MyRobot
 
     public void teleopInit()
     {
-        System.out.println("Synchronizing pneumatics");
-        wrist.lower();
+        System.out.println("***Synchronizing pneumatics***");
+        System.out.println("Force lowering wrist");
+        wrist.forceLower();
         Timer.delay(1.0);
-        wrist.raise();
+        System.out.println("Force raising wrist");
+        wrist.forceRaise();
     }
 
     public void teleopPeriodic()
     {
         if(xbox.getRawButton(Xbox.Button.kA))
         {
+            System.out.println("Input button A");
+            System.out.println("Wrist lowering");
             wrist.lower();
         }
         else if(xbox.getRawButton(Xbox.Button.kB))
         {
+            System.out.println("Input button B");
+            System.out.println("Wrist raising");
             wrist.raise();
         }
+        else if(xbox.getRawButton(Xbox.Button.kX))
+        {
+            System.out.println("Input button X");
+            System.out.println("Force lowering wrist");
+            wrist.forceLower();
+        }
+        else if(xbox.getRawButton(Xbox.Button.kY))
+        {
+            System.out.println("Input button Y");
+            System.out.println("Force raising wrist");
+            wrist.forceRaise();
+        }
+        else if(xbox.getRawButton(Xbox.Button.kStart))
+        {
+            if(newTimeOut == 10.0)
+            {
+                System.out.println("Time out upper limit reached at 10.0s");
+            }
+            else
+            {
+                newTimeOut = newTimeOut + 0.5;
+                System.out.println("Increasing the new time out to " + Double.toString(newTimeOut) + "s");
+            }
+        }
+        else if(xbox.getRawButton(Xbox.Button.kBack))
+        {
+            if(newTimeOut == 1.0)
+            {
+                System.out.println("Time out lower limit reached at 1.0s");
+            }
+            else
+            {
+                newTimeOut = newTimeOut - 0.5;
+                System.out.println("Decreasing the new time out to " + Double.toString(newTimeOut) + "s");
+            }
+        }
+        else if(xbox.getRawButton(Xbox.Button.kLB))
+        {
+            System.out.println("Setting new lowering time out at " + Double.toString(newTimeOut) + "s");
+            wrist.setLoweringTimeOut(newTimeOut);
+        }
+        else if(xbox.getRawButton(Xbox.Button.kRB))
+        {
+            System.out.println("Setting new raising time out at " + Double.toString(newTimeOut) + "s");
+            wrist.setRaisingTimeOut(newTimeOut);
+        }
 
+        if(wrist.isUp())
+        {
+            System.out.println("Arm is up");
+        }
+        else if(wrist.isDown())
+        {
+            System.out.println("Arm is down");
+        }
     }
 
     public void testInit()
@@ -63,7 +124,6 @@ public class MyRobot
 
     public void testPeriodic()
     {
-
     }
 
     public void disabledInit()
--- src/main/java/frc/darren/Wrist.java
+++ src/main/java/frc/darren/Wrist.java
@@ -1,6 +1,7 @@
 package frc.darren;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Timer;
 
 /**
  * Class for controlling the Wrist of the Intake system.
@@ -8,7 +9,13 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
  */
 public class Wrist
 {
-    // Constants - Change these if the solenoids are going in the wrong direction
+    // Constants
+    private static enum WristState
+    {
+        kUp, kLowering, kDown, kRaising
+    };
+
+    // TODO: Change these if the solenoids are going in the wrong direction
     private static final int WRIST_SOLENOID_LEFT_EXTEND_PORT = 1;
     private static final int WRIST_SOLENOID_LEFT_RETRACT_PORT = 0;
     private static final int WRIST_SOLENOID_RIGHT_EXTEND_PORT = 2;
@@ -17,11 +24,17 @@ public class Wrist
     private static DoubleSolenoid wristSolenoidLeft = new DoubleSolenoid(WRIST_SOLENOID_LEFT_EXTEND_PORT, WRIST_SOLENOID_LEFT_RETRACT_PORT);
     private static DoubleSolenoid wristSolenoidRight = new DoubleSolenoid(WRIST_SOLENOID_RIGHT_EXTEND_PORT, WRIST_SOLENOID_RIGHT_RETRACT_PORT);
 
+    private WristState wristState = WristState.kUp;
+    private static Timer timer;
+    private double loweringTimeOut = 1.0;
+    private double raisingTimeOut = 1.0;
+
     private static Wrist instance = new Wrist();
 
     private Wrist()
     {
         System.out.println(this.getClass().getName() + ": Started Constructing");
+        timer = new Timer();
         System.out.println(this.getClass().getName() + ": Finished Constructing");
     }
 
@@ -31,19 +44,111 @@ public class Wrist
     }
 
     /**
-     * Lowers the Wrist on the Intake system.
-     */
-    public void lower()
-    {
-        setPneumatics(DoubleSolenoid.Value.kForward);
-    }
-
-    /**
      * Raises the Wrist on the Intake system.
      */
     public void raise()
     {
+        if(wristState !=  WristState.kUp && wristState != WristState.kRaising)
+        {
+            timer.reset();
+            setPneumatics(DoubleSolenoid.Value.kReverse);
+            wristState = WristState.kRaising;
+            timer.start();
+        }
+    }
+
+    /**
+     * Checks if the Wrist is up or should be up by this time.
+     * @return Whether the Wrist is up (true) or not.
+     */
+    public boolean isUp()
+    {
+        if(wristState == WristState.kUp)
+        {
+            return true;
+        }
+        else if((wristState == WristState.kRaising) && (timer.get() >= raisingTimeOut)) // TODO: Check limit switch possibly
+        {
+            wristState = WristState.kUp;
+            timer.stop();
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /**
+     * Lowers the Wrist on the Intake system.
+     */
+    public void lower()
+    {
+        if(wristState != WristState.kDown && wristState != WristState.kLowering)
+        {
+            timer.reset();
+            setPneumatics(DoubleSolenoid.Value.kForward);
+            wristState = WristState.kLowering;
+            timer.start();
+        }
+    }
+
+    /**
+     * Checks if the Wrist is down or should be down by this time.
+     * @return Whether the Wrist is down (true) or not.
+     */
+    public boolean isDown()
+    {
+        if(wristState == WristState.kDown)
+        {
+            return true;
+        }
+        else if((wristState == WristState.kLowering) && (timer.get() >= loweringTimeOut)) // TODO: Check limit switch possibly
+        {
+            wristState = WristState.kDown;
+            timer.stop();
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /**
+     * Synchronizes the position with the state by force raising the Wrist.
+     */
+    public void forceRaise()
+    {
         setPneumatics(DoubleSolenoid.Value.kReverse);
+        wristState = WristState.kUp;
+    }
+
+    /**
+     * Synchronizes the position with the state by force lowering the Wrist.
+     */
+    public void forceLower()
+    {
+        setPneumatics(DoubleSolenoid.Value.kForward);
+        wristState = WristState.kDown;
+    }
+
+    /**
+     * Sets the time out for lowering the Wrist.
+     * @param newTimeOut
+     */
+    public void setLoweringTimeOut(double newTimeOut)
+    {
+        loweringTimeOut = newTimeOut;
+    }
+
+    /**
+     * Sets the time out for raising the Wrist.
+     * @param newTimeOut
+     */
+    public void setRaisingTimeOut(double newTimeOut)
+    {
+        raisingTimeOut = newTimeOut;
     }
 
     /**
