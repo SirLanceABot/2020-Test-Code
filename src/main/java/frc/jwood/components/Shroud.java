04d9b4cca538d5c96fe98a12ae5bfa2fc397563f
Jon Woodard <jwoodard@lakeshoreps.org>
2020-02-21 14:49:41 -0500

    Added Shroud - jw

---
+++ src/main/java/frc/jwood/components/Shroud.java
@@ -0,0 +1,41 @@
+package frc.jwood.components;
+
+// import wpilibj.Bosch;
+
+/**
+ * Class for controlling the Shroud of the Shooter.
+ * @author Darren Fife
+ */
+public class Shroud
+{
+    private double previousSpeed = 0;
+
+    private static Shroud instance = new Shroud();
+
+    private Shroud()
+    {
+        System.out.println(this.getClass().getName() + ": Started Constructing");
+        // Bosch(CANTalonDeviceID, encoderPort);
+
+        System.out.println(this.getClass().getName() + ": Finished Constructing");
+    }
+
+    protected static Shroud getInstance()
+    {
+        return instance;
+    }
+
+    private double checkDirectionChange(double newSpeed)
+    {
+        // Update position accumulator if changing direction
+        // Encoder doesn't know the direction so we have to remember the direction for it
+        if((previousSpeed < 0 && newSpeed >= 0) || (previousSpeed >= 0 && newSpeed < 0))
+        {
+            // position = motor.getPosition();
+            // counter.reset();
+            previousSpeed = newSpeed;
+        }
+
+        return newSpeed;
+    }
+}
