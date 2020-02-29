329833939f25d1ab7ab3ee01aee5c8df0b91e9cc
= <=>
2020-02-29 15:22:12 -0500

    updated lidar - ap

--- src/main/java/frc/annika/Constants.java
+++ src/main/java/frc/annika/Constants.java
@@ -8,7 +8,7 @@ public final class Constants
 	public final static class LIDAR 
 	{
 
-		public static final I2C.Port PORT = I2C.Port.kOnboard; //I2C.Port.kMXP select roboRIO I2C port
+		public static final I2C.Port PORT = I2C.Port.kMXP; //I2C.Port.kOnboard select roboRIO I2C port
 		public static final LIDAR_Lite.Address ADDRESS = LIDAR_Lite.Address.DEFAULT;	// I2C address on selected port
 		private LIDAR() 
 		{
